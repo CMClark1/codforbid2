e25d30412a33d99dcdbe95bd6856342df05b66f9
CMClark1 <caira.clark@dfo-mpo.gc.ca>
2025-03-07 12:02:51 -0400

    roxygen description, clean names

--- R/marfispull.R
+++ R/marfispull.R
@@ -1,5 +1,16 @@
+#'@title marfispull
+#'@description Pull landings data for the groundfish fleet from DFO's Maritime Fishery Information System (MARFIS) database.
+#'@param year The year required.
+#'@param username Oracle username. Default is the value oracle.username stored in .Rprofile.
+#'@param password Oracle password. Default is the value oracle.password stored in .Rprofile.
+#'@param dsn Oracle dsn. Default is the value oracle.dsn stored in .Rprofile.
+#'@return a data.frame of landings
+#'@examples \dontrun{example <- marfispull(year=2024)}
+#'@export
+
 
 marfispull <- function(year=NULL,username=oracle.username,password=oracle.password,dsn=oracle.dsn){
+
   channel <- ROracle::dbConnect(DBI::dbDriver("Oracle"), username=username, password=password, dsn)
   marfis1 <- ROracle::dbGetQuery(channel, paste("
 
@@ -46,10 +57,13 @@ where a.vr_number_fishing=b.vr_number
 
   obs_trip <- ROracle::dbGetQuery(channel, "select a.mon_doc_id, a.column_defn_id, a.data_value TRIP from marfissci.mon_doc_entrd_dets a where a.column_defn_id=741")
 
+  oracle.username <- oracle.password <- oracle.dsn <- RND_WEIGHT_KGS <- MON_DOC_ID <- COLUMN_DEFN_ID <- SPECIES_CODE <- SumOfRND_WEIGHT_KGS <- NULL
+
   marfis <- marfis1 |>
     dplyr::left_join(obs_trip) |>
     dplyr::summarise(SumOfRND_WEIGHT_KGS=sum(RND_WEIGHT_KGS), .by=c(-MON_DOC_ID,-COLUMN_DEFN_ID,-RND_WEIGHT_KGS)) |>
-    tidyr::pivot_wider(names_from=SPECIES_CODE, values_from=SumOfRND_WEIGHT_KGS)
+    tidyr::pivot_wider(names_from=SPECIES_CODE, values_from=SumOfRND_WEIGHT_KGS) |>
+    janitor::clean_names()
 
   print(marfis)
 
