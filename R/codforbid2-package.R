9489d83959ad51bbd897c50a6a01acd391d74049
CMClark1 <caira.clark@dfo-mpo.gc.ca>
2025-03-07 12:55:34 -0400

    initial commit

---
+++ R/codforbid2-package.R
@@ -0,0 +1,12 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom dplyr left_join
+#' @importFrom dplyr summarise
+#' @importFrom janitor clean_names
+#' @importFrom ROracle dbConnect
+#' @importFrom ROracle dbGetQuery
+#' @importFrom tidyr pivot_wider
+## usethis namespace: end
+NULL
