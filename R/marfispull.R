fe5b2dcacbb1f1ad18fc974cc3579365668c9a7f
CMClark1 <caira.clark@dfo-mpo.gc.ca>
2025-03-07 12:57:19 -0400

    styler

--- R/marfispull.R
+++ R/marfispull.R
@@ -1,17 +1,18 @@
-#'@title marfispull
-#'@description Pull landings data for the groundfish fleet from DFO's Maritime Fishery Information System (MARFIS) database.
-#'@param year The year required.
-#'@param username Oracle username. Default is the value oracle.username stored in .Rprofile.
-#'@param password Oracle password. Default is the value oracle.password stored in .Rprofile.
-#'@param dsn Oracle dsn. Default is the value oracle.dsn stored in .Rprofile.
-#'@return a data.frame of landings
-#'@examples \dontrun{example <- marfispull(year=2024)}
-#'@export
+#' @title marfispull
+#' @description Pull landings data for the groundfish fleet from DFO's Maritime Fishery Information System (MARFIS) database.
+#' @param year The year required.
+#' @param username Oracle username. Default is the value oracle.username stored in .Rprofile.
+#' @param password Oracle password. Default is the value oracle.password stored in .Rprofile.
+#' @param dsn Oracle dsn. Default is the value oracle.dsn stored in .Rprofile.
+#' @return a data.frame of landings
+#' @examples \dontrun{
+#' example <- marfispull(year = 2024)
+#' }
+#' @export
 
 
-marfispull <- function(year=NULL,username=oracle.username,password=oracle.password,dsn=oracle.dsn){
-
-  channel <- ROracle::dbConnect(DBI::dbDriver("Oracle"), username=username, password=password, dsn)
+marfispull <- function(year = NULL, username = oracle.username, password = oracle.password, dsn = oracle.dsn) {
+  channel <- ROracle::dbConnect(DBI::dbDriver("Oracle"), username = username, password = password, dsn)
   marfis1 <- ROracle::dbGetQuery(channel, paste("
 
 select EXTRACT(YEAR FROM a.LANDED_DATE) YEAR,
@@ -51,9 +52,9 @@ where a.vr_number_fishing=b.vr_number
         and d.fleet_quota_id=h.fleet_quota_id
         and c.area like '5Z%'
         and a.species_code in (100,110,170)
-        and EXTRACT(YEAR FROM a.LANDED_DATE)=",year,"
+        and EXTRACT(YEAR FROM a.LANDED_DATE)=", year, "
         and TO_CHAR(a.date_fished,'Q') in (1,2,3,4)
-", sep=" "))
+", sep = " "))
 
   obs_trip <- ROracle::dbGetQuery(channel, "select a.mon_doc_id, a.column_defn_id, a.data_value TRIP from marfissci.mon_doc_entrd_dets a where a.column_defn_id=741")
 
@@ -61,10 +62,9 @@ where a.vr_number_fishing=b.vr_number
 
   marfis <- marfis1 |>
     dplyr::left_join(obs_trip) |>
-    dplyr::summarise(SumOfRND_WEIGHT_KGS=sum(RND_WEIGHT_KGS), .by=c(-MON_DOC_ID,-COLUMN_DEFN_ID,-RND_WEIGHT_KGS)) |>
-    tidyr::pivot_wider(names_from=SPECIES_CODE, values_from=SumOfRND_WEIGHT_KGS) |>
+    dplyr::summarise(SumOfRND_WEIGHT_KGS = sum(RND_WEIGHT_KGS), .by = c(-MON_DOC_ID, -COLUMN_DEFN_ID, -RND_WEIGHT_KGS)) |>
+    tidyr::pivot_wider(names_from = SPECIES_CODE, values_from = SumOfRND_WEIGHT_KGS) |>
     janitor::clean_names()
 
   print(marfis)
-
 }
