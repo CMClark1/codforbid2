f60914367bbe169439af9b5bc241407c0d996eba
CMClark1 <caira.clark@dfo-mpo.gc.ca>
2025-03-11 14:57:29 +0100

    more importfroms

--- R/codforbid2-package.R
+++ R/codforbid2-package.R
@@ -3,8 +3,12 @@
 
 ## usethis namespace: start
 #' @importFrom dplyr left_join
+#' @importFrom dplyr mutate
+#' @importFrom dplyr rename
+#' @importFrom dplyr select
 #' @importFrom dplyr summarise
 #' @importFrom janitor clean_names
+#' @importFrom Mar.utils identify_area
 #' @importFrom ROracle dbConnect
 #' @importFrom ROracle dbGetQuery
 #' @importFrom tidyr pivot_wider
